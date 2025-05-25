c66f2f78d3504cfd9fc49a2bba57acba43679437
luo <luoyunfu41@gmail.com>
2025-05-25 14:34:25 +0800

    discussion 2

--- discussion/src/rotate.java
+++ discussion/src/rotate.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class rotate {
+
+    public static int[] Rotate(int[] A, int i) {
+        int rightShift = i;
+        if (rightShift > A.length) {
+            rightShift = i % A.length;
+        }
+        int[] newArr = new int[A.length];
+        for (int j = 0; j < A.length; j++) {
+            newArr[(rightShift + j) % A.length] = A[j];
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5,6};
+        int[] a = Rotate(A, 3);
+        System.out.println(a[0]);
+    }
 }
